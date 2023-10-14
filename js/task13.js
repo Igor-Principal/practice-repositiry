91960bf26d34711532d2e7f7d0aa90ff3c32de5d
Igor-Principal <igorprincipal@gmail.com>
2023-10-14 16:30:13 +0300

    Task 13

---
+++ js/task13.js
@@ -0,0 +1,29 @@
+/**
+ *? Напиши клас Client який створює об'єкт
+ *? з ​​властивостями login email
+ *? Оголоси приватні властивості #login #email,
+ *? доступ до яких зроби через геттер та сеттер login email
+ */
+// const client = new Client("mango", "mango@gmail.com");
+
+class Client {
+    #login;
+    #email;
+    constructor(login, email) {
+        this.#login = login;
+        this.#email = email;
+    }
+
+    get clientData() {
+        return { login: this.#login, email: this.#email };
+    }
+    set clientData({ newLogin, newEmail}) {
+    this.#login = newLogin;
+    this.#email = newEmail;
+  }
+}
+const client = new Client("mango", "mango@gmail.com");
+console.log(client.clientData);
+client.clientData = {newLogin: "apple", newEmail: "apple@gmail.com"}
+console.log(client.clientData);
+
